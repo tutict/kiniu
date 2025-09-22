3e9647b18dce1b6e5dc9939746c5641f9e9b3047
tutict <tutict@163.com>
2025-09-22 15:21:53 +0800

    feat(init): 初始化项目结构和基础配置
    添加了后端 Spring Boot项目和前端 Nuxt.js 项目的初始文件，包括：- 后端：Maven 配置、Spring Boot 启动类、基本依赖（Lombok、测试库等）- 前端：Nuxt.js 基础页面、配置文件、依赖项及开发工具设置
    - 公共：.gitignore 文件、README 文档、构建脚本目录

---
+++ kiniu-back/src/main/java/com/tutict/kiniuback/KiniuBackApplication.java
@@ -0,0 +1,13 @@
+package com.tutict.kiniuback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiniuBackApplication {
+
+    static void main() {
+        SpringApplication.run(KiniuBackApplication.class);
+    }
+
+}
---
+++ kiniu-back/src/test/java/com/tutict/kiniuback/KiniuBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutict.kiniuback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiniuBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
